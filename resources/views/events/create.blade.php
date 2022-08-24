8a8578dd696a50e3b335575ac2ae7c620676a8c9
Victor Kauan <victorkauan.dev@gmail.com>
2022-08-24 14:22:32 -0300

    feat and fix: load event images (add a new column in events table) and fix event layout on home page

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -5,25 +5,29 @@
 @section('content')
     <div id="event-create-container" class="col-md-6 offset-md-3">
         <h1>Create your event</h1>
-        <form action="/events" method="POST">
+        <form action="/events" method="POST" enctype="multipart/form-data">
             @csrf
+            <div class="form-group">
+                <label for="image">Event image:</label>
+                <input type="file" class="form-control-file" id="image" name="image" />
+            </div>
             <div class="form-group">
                 <label for="title">Event:</label>
                 <input type="text" class="form-control" id="title" name="title" placeholder="Event name" />
             </div>
             <div class="form-group">
-                <label for="title">City:</label>
+                <label for="city">City:</label>
                 <input type="text" class="form-control" id="city" name="city" placeholder="Event's place" />
             </div>
             <div class="form-group">
-                <label for="title">Is the event private?</label>
+                <label for="private">Is the event private?</label>
                 <select name="private" id="private" class="form-control">
                     <option value="0">No</option>
                     <option value="1">Yes</option>
                 </select>
             </div>
             <div class="form-group">
-                <label for="title">Description:</label>
+                <label for="description">Description:</label>
                 <textarea class="form-control" name="description" id="description" placeholder="What will happen at the event?"></textarea>
             </div>
             <input type="submit" class="btn btn-primary" value="Create Event" />

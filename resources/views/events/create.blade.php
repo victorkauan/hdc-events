a60d83a91179e6d3a405f49b7fb433adebd707fa
Victor Kauan <victorkauan.dev@gmail.com>
2022-08-24 13:35:33 -0300

    feat: create event creation page form, POST route /events and store action in EventController

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -3,5 +3,30 @@
 @section('title', 'Create Event')
 
 @section('content')
-    <h1>Create an event!</h1>
+    <div id="event-create-container" class="col-md-6 offset-md-3">
+        <h1>Create your event</h1>
+        <form action="/events" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="title">Event:</label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="Event name" />
+            </div>
+            <div class="form-group">
+                <label for="title">City:</label>
+                <input type="text" class="form-control" id="city" name="city" placeholder="Event's place" />
+            </div>
+            <div class="form-group">
+                <label for="title">Is the event private?</label>
+                <select name="private" id="private" class="form-control">
+                    <option value="0">No</option>
+                    <option value="1">Yes</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="title">Description:</label>
+                <textarea class="form-control" name="description" id="description" placeholder="What will happen at the event?"></textarea>
+            </div>
+            <input type="submit" class="btn btn-primary" value="Create Event" />
+        </form>
+    </div>
 @endsection

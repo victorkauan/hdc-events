e707bfeaa0c4ab0dfbbc6aabfa384b9318e294cd
Victor Kauan <victorkauan.dev@gmail.com>
2022-08-24 04:03:59 -0300

    feat: add static files

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,2 +1,16 @@
-<h1>This is the contact page</h1>
-<a href="/">Go back to home</a>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/scripts.js"></script>
+    </head>
+    <body>
+        <h1>This is the contact page</h1>
+        <a href="/">Go back to home</a>
+    </body>
+</html>

8d3cdb1049709205055ecd8b26528d34cdb12dab
Victor Kauan <victorkauan.dev@gmail.com>
2022-08-25 04:20:52 -0300

    feat: display user event list on dashboard and create validation for event editing

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -68,7 +68,9 @@ class EventController extends Controller
 
         $events = $user->events;
 
-        return view('events.dashboard', ['events' => $events]);
+        $eventsAsParticipant = $user->eventsAsParticipant;
+
+        return view('events.dashboard', ['events' => $events, 'eventsAsParticipant' => $eventsAsParticipant]);
     }
 
     public function destroy($id) {
@@ -78,7 +80,13 @@ class EventController extends Controller
     }
 
     public function edit($id) {
+        $user = auth()->user();
+
         $event = Event::findOrFail($id);
+
+        if ($user->id != $event->user_id) {
+            return redirect('/dashboard');
+        }
 
         return view('events.edit', ['event' => $event]);
     }
--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -22,7 +22,7 @@
                         <tr>
                             <td scope="row">{{ $loop->index + 1 }}</td>
                             <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
-                            <td>0</td>
+                            <td>{{ count($event->users) }}</td>
                             <td>
                                 <a href="/events/edit/{{ $event->id }}" class="btn btn-info edit-btn"><ion-icon name="create-outline"></ion-icon> Edit</a>
                                 <form action="/events/{{ $event->id }}" method="POST">
@@ -37,6 +37,37 @@
             </table>
         @else
             <p>You have no events yet, <a href="/events/create">create event</a></p>
+        @endif
+    </div>
+    <div class="col-md-10 offset-md-1 dashboard-title-container">
+        <h1>Events I'm participating in:</h1>
+    </div>
+    <div class="col-md-10 offset-md-1 dashboard-event-container">
+        @if (count($eventsAsParticipant))
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Participants</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($eventsAsParticipant as $event)
+                        <tr>
+                            <td scope="row">{{ $loop->index + 1 }}</td>
+                            <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
+                            <td>{{ count($event->users) }}</td>
+                            <td>
+                                <a href="#">Leave the event</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @else
+            <p>You're not participating in any events yet, <a href="/">see all events</a></p>
         @endif
     </div>
 @endsection
